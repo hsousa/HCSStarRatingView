fcf0bd230a44c5cc54ec40ba06c2f9b890a1edb9
LinXi <xi.lin@maitao.com>
2016-03-30 17:16:10 +0800

    avoid double init

--- HCSStarRatingView/HCSStarRatingView.m
+++ HCSStarRatingView/HCSStarRatingView.m
@@ -39,14 +39,6 @@
 
 #pragma mark - Initialization
 
-- (instancetype)init {
-    self = [super init];
-    if (self) {
-        [self _customInit];
-    }
-    return self;
-}
-
 - (instancetype)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
